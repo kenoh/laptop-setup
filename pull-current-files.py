c2d42d7cb4368209be48181568c09e82997a5577
Matúš Honěk <mhonek@redhat.com>
2019-02-19 13:36:14 +0100

    pull-current-files.py: Fix pep8 errors

--- pull-current-files.py
+++ pull-current-files.py
@@ -4,18 +4,16 @@ import click
 import subprocess as sp
 import difflib
 
+
 class Role():
     def __init__(self, role):
         self.role = role
 
-
     def __str__(self):
         return "<Role %s>" % str(self.role)
 
-
     def _get_target_file_path(self, pull_file):
         return pl.Path(*pull_file.parts[:-1], pull_file.stem)
-
 
     def _file_changed(self, pull_file):
         target_file = self._get_target_file_path(pull_file)
@@ -23,31 +21,28 @@ class Role():
             new = sp.check_output(str(pull_file), shell=True, stderr=sp.PIPE).decode().splitlines(True)
             try:
                 old = open(target_file).readlines()
-            except FileNotFoundError as e:
+            except FileNotFoundError:
                 print("## %s: tracked file not found at `%s`, expecting it being a new." % (pull_file, target_file))
                 old = []
             diff = list(difflib.unified_diff(old, new,
                                              fromfile="a/" + str(target_file),
-                                             tofile="b/" + str(target_file)) )
+                                             tofile="b/" + str(target_file)))
             return diff
         except sp.CalledProcessError as e:
             print("## %s: could not run the .pull file, exception was: " % (pull_file), e)
             return False
 
-
     def _do_process(self, cb):
         pull_files = list(pl.Path(self.role, 'files').glob('*.pull'))
         for pull_file in pull_files:
             diff = self._file_changed(pull_file)
-            if diff != False:
+            if diff is not False:
                 cb(pull_file, diff)
-
 
     def examine(self):
         def cb(pull_file, diff):
             print('## %s: up to date.' % pull_file.name if diff == [] else ''.join(diff))
         return self._do_process(cb)
-
 
     def _do_pull_file(self, pull_file):
         target_file = self._get_target_file_path(pull_file)
@@ -65,13 +60,11 @@ class Role():
             return False
         assert False, "shouldn't have gotten here"
 
-
     def pull(self):
         def cb(pull_file, diff):
             if diff != []:
                 return self._do_pull_file(pull_file)
         return self._do_process(cb)
-
 
     def _do_write_diff_file(self, pull_file, diff):
         target_file = pull_file.with_suffix(".pull.diff")
@@ -80,7 +73,6 @@ class Role():
             print("## %s: diff file written in %s." % (pull_file, target_file))
         return True
 
-
     def write_diffs(self):
         def cb(pull_file, diff):
             if diff != []:
@@ -88,7 +80,9 @@ class Role():
         return self._do_process(cb)
 
 
-METHODS = {'overwrite': Role.pull, 'write-diffs': Role.write_diffs, 'examine': Role.examine }
+METHODS = {'overwrite': Role.pull, 'write-diffs': Role.write_diffs, 'examine': Role.examine}
+
+
 @click.command()
 @click.argument('method', required=True, type=click.Choice(METHODS.keys()), default='examine')
 @click.argument('role', nargs=-1, required=False, type=str)
@@ -96,12 +90,12 @@ def main(method, role):
     """Program for getting modified locally deployed files via Ansible roles back to the roles' trees for further availability to the user (usually to be versioned in git, etc.).
 
     Every script found at `roles/*/files/*.pull` will be run and, depending on the METHOD, its standard output (stdout) will be:
-    
+
     \b
     - printed to stdout (METHOD 'examine', the default)
     - diffed against the respective tracked file and written to a nerby .pull.diff file (METHOD 'write-diffs')
     - written to the respective tracked file overwriting it (METHOD 'overwrite')
-    
+
     You can either specify ROLEs explicitly or leave it blank so that all available roles are processed.
 
     Simplest example of a .pull script could look like this:
@@ -110,7 +104,7 @@ def main(method, role):
     """
     method = METHODS[method]
     roles_dir = pl.Path('roles')
-    roles = [ Role(pl.Path(roles_dir, r)) for r in role ] if role else map(Role, list(roles_dir.iterdir()))
+    roles = [Role(pl.Path(roles_dir, r)) for r in role] if role else map(Role, list(roles_dir.iterdir()))
     for role_obj in roles:
         print('# %s' % role_obj)
         method(role_obj)
