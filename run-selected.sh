f6847004ee0b30d8dfd53bf1ee90d5a38b06a72b
Matus Honek <matushonek@gmail.com>
2019-10-13 13:16:37 +0200

    Fix run-selected.sh annoyances with missing files to remove

--- run-selected.sh
+++ run-selected.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-set -e
 set -x
 
 FN="test.yml"
 FNR="test.retry"
 
-test -f "$FN" && exit 1
+test -f "$FN" && (
+  echo "Found test.yml file. Is it yours? If so, remove it before running again. Thanks!"
+  exit 1
+)
 
 cat > "$FN" <<EOF
 ---
@@ -15,7 +17,6 @@ cat > "$FN" <<EOF
 $(echo ${@} | sed 's/ /\n/g' | sed 's/^/    - /')
 EOF
 
-ansible-playbook --ask-become-pass -i inventory.txt --diff ${CHECK+--check} ${VERBOSE} "$FN" \
-	|| { rm "$FN" "$FNR" ; exit 1 ; }
+ansible-playbook --ask-become-pass -i inventory.txt --diff ${CHECK+--check} ${VERBOSE} "$FN"
 
-rm "$FN"
+rm "$FN" "$FNR"
